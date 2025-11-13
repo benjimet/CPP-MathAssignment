a7ce07271abef8c7cd4ed003562ee3c402b2ef64
benji(met) <st20346682@outlook.cardiffmet.ac.uk>
2025-11-13 15:07:54 +0000

    Chapter 4 Progress

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -2,6 +2,8 @@
 
 // Function prototypes
 
+void myUpdate(GLFWwindow* window, double tDelta);
+
 
 int main(void) {
 
@@ -19,8 +21,32 @@ int main(void) {
 	// Setup game scene objects here
 	//
 	
+	addObject("player");
+	addObject("player2");
+
+	GameObject2D* player1Object = getObject("player");
+
+
+	if (player1Object != nullptr) {
+		player1Object->position = glm::vec2(-1.0f, 1.0f);
+		player1Object->orientation = glm::radians(45.0f);
+		player1Object->textureID = loadTexture("Resources\\Textures\\bumblebee.png");
+
+	}
+
+	GameObject2D* player2Object = getObject("player2");
+
+	if (player2Object != nullptr) {
+		player2Object->position = glm::vec2(1.5f, 1.0f);
+		player2Object->orientation = glm::radians(30.0f);
+		player2Object->size = glm::vec2(3.0f, 3.0f);
+
+	}
+
+
 
 	// Enter main loop - this handles update and render calls
+	setUpdateFunction(myUpdate);
 	engineMainLoop();
 
 	// When we quit (close window for example), clean up engine resources
@@ -28,6 +54,15 @@ int main(void) {
 
 	// return success :)
 	return 0;
+}
+
+void myUpdate(GLFWwindow* window, double tDelta) {
+	GameObject2D* player = getObject("player");
+	float anglesPerSecond = glm::radians(45.0f);
+	float playerVelocity = 2.0f;
+	float playerRotationSpeed = glm::radians(90.0f);
+	player->orientation += playerRotationSpeed * tDelta;
+	
 }
 
 
